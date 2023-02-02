521b633094f3af889f3af6d3bb33b8e2a9400777
xlboy <xlboy88888@foxmail.com>
2023-02-02 16:35:15 +0800

    feat(1-base): init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # learn-web-debug
+
+## 目录
+
+1. [调试基本的 **web-server**](./packages/1-base/)
---
+++ packages/1-base/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function btnClick() {
+    const i = 0;
+    debugger;
+    console.log("print-");
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={btnClick}>count is {count}</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
